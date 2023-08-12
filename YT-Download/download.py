7034bd6601e20217d4876f0e92709aeeacce924f
cbrig5 <cbrig5@brockport.edu>
2023-08-12 15:14:10 -0400

    [ADD] YT-Download: Adds a youtube api authentication function

--- YT-Download/download.py
+++ YT-Download/download.py
@@ -15,6 +15,33 @@ import pickle
 
 SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
+def youtube_authenticate():
+    '''Performs OAuth 2.0 authentication with googleapis.com.'''
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    api_service_name = "youtube"
+    api_version = "v3" 
+    client_secrets_file = "credentials.json"
+    creds = None
+    # the file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # if there are no (valid) credentials availablle, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    return build(api_service_name, api_version, credentials=creds)
+
+youtube_authenticate = youtube_authenticate()
+
 def download_with_progress(stream, file_path):
     response = urlopen(stream.url)
     total_size = int(response.getheader('content-length'))
