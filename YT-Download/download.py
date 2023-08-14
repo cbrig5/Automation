a5d0f12eab234bd9e986a7e368684bdaa5f981fa
cbrig5 <cbrig5@brockport.edu>
2023-08-14 09:31:50 -0400

    [ADD] YT-Download: Adds functions to get channel id, playlist id and video details

--- YT-Download/download.py
+++ YT-Download/download.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import requests
 
 import urllib.parse as p
 import re
@@ -15,11 +16,12 @@ import pickle
 
 SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
+
 def youtube_authenticate():
-    '''Performs OAuth 2.0 authentication with googleapis.com.'''
+    """Performs OAuth 2.0 authentication with googleapis.com."""
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     api_service_name = "youtube"
-    api_version = "v3" 
+    api_version = "v3"
     client_secrets_file = "credentials.json"
     creds = None
     # the file token.pickle stores the user's access and refresh tokens, and is
@@ -32,7 +34,9 @@ def youtube_authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file, SCOPES
+            )
             creds = flow.run_local_server(port=0)
         # save the credentials for the next run
         with open("token.pickle", "wb") as token:
@@ -40,44 +44,157 @@ def youtube_authenticate():
 
     return build(api_service_name, api_version, credentials=creds)
 
-youtube_authenticate = youtube_authenticate()
+
+def search(youtube, **kwargs):
+    return youtube.search().list(part="snippet", **kwargs).execute()
+
+
+def get_user_id(youtube):
+    return youtube.channels().list(part="id", mine=True).execute()["items"][0]["id"]
+
+
+def get_playlist_id(youtube, channel_id, playlist_name):
+    for i in (
+        youtube.playlists()
+        .list(part="snippet", channelId=channel_id)
+        .execute()
+        .get("items")
+    ):
+        if i["snippet"]["title"] == playlist_name:
+            return i["id"]
+
+    raise Exception(f"Cannot find playlist:{playlist_name}")
+
+
+def get_videos_from_playlist(youtube, playlist_id):
+    request = youtube.playlistItems().list(
+        part="snippet", playlistId=playlist_id, maxResults=50
+    )
+
+    playlist_videos = []
+
+    while request:
+        response = request.execute()
+        playlist_videos += response["items"]
+        request = youtube.playlistItems().list_next(request, response)
+
+    return playlist_videos
+
+
+def get_video_from_id(youtube, video_id):
+    request = youtube.videos().list(part="snippet", id=video_id)
+
+    response = request.execute()
+
+    return response["items"]
+
+
+def get_video_details(videos, video_id):
+    video_information = {}
+    sub_details = {}
+
+    for video in videos:
+        video_id = video_id or video["snippet"]["resourceId"]["videoId"]
+        video_channel_title = video["snippet"].get(
+            "VideoOwnerChannelTitle", video["snippet"]["channelTitle"]
+        )
+        video_title = video["snippet"]["title"]
+        video_description = video["snippet"]["description"]
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+        sub_details.update(
+            {
+                "video_channel_title": video_channel_title,
+                "video_title": video_title,
+                "video_description": video_description,
+                "video_url": video_url,
+            }
+        )
+
+        video_information[video_id] = sub_details
+
+    if video_information:
+        return video_information
+    else:
+        raise Exception("No videos found")
+
 
 def download_with_progress(stream, file_path):
     response = urlopen(stream.url)
-    total_size = int(response.getheader('content-length'))
+    total_size = int(response.getheader("content-length"))
 
-    with tqdm(total=total_size, unit='B', unit_scale=True, unit_divisor=1024, desc=stream.default_filename, ascii=True) as pbar:
-        with open(file_path, 'wb') as file:
+    with tqdm(
+        total=total_size,
+        unit="B",
+        unit_scale=True,
+        unit_divisor=1024,
+        desc=stream.default_filename,
+        ascii=True,
+    ) as pbar:
+        with open(file_path, "wb") as file:
             chunk_size = 1024 * 256  # 256 KB chunks
-            for chunk in iter(lambda: response.read(chunk_size), b''):
+            for chunk in iter(lambda: response.read(chunk_size), b""):
                 file.write(chunk)
                 pbar.update(len(chunk))
 
-video_url = "https://www.youtube.com/watch?v=PJg_rnK7TFo"
 
-video = YouTube(video_url)
-video_stream = video.streams.get_highest_resolution()
+def main():
+    youtube = youtube_authenticate()
 
-title = video.title
+    # user choice to download from own channel or another channel
+    channel_type = input(
+        "Enter 1 to download from your own channel or 2 to download from another channel: "
+    )
 
-download_path = "./Downloads/Markiplier"
+    if channel_type == "1":
+        channel_id = get_user_id(youtube)
+    else:
+        channel_id = input("Enter the channel id: ")
+
+    video_id = None
+
+    download_type = input(
+        "Enter 1 to download a single video or 2 to download from a playlist : "
+    )
+    if download_type == "1":
+        video_id = input("Enter the video id: ")
+        video = get_video_from_id(youtube, video_id)
+
+    else:
+        playlist_name = input("Enter the playlist name: ")
+        playlist_id = get_playlist_id(youtube, channel_id, playlist_name)
+        video = get_videos_from_playlist(youtube, playlist_id)
+
+    video_details = get_video_details(video, video_id)
+    print(video_details)
+
+    # video_url = "https://www.youtube.com/watch?v=PJg_rnK7TFo"
+    # video = YouTube(video_url)
+    # video_stream = video.streams.get_highest_resolution()
+
+    # title = video.title
+
+    # download_path = "./Downloads/Markiplier"
+
+    try:
+        start_time = time.time()
+        # download_with_progress(video_stream, f"{download_path}/{title}.mp4")
+        end_time = time.time()
+
+        total_time = end_time - start_time
+
+        # Convert total time to minutes and seconds
+        elapsed_minutes = int(total_time // 60)
+        elapsed_seconds = int(total_time % 60)
+
+        print("Download Completed!")
+        print(f"Download time: {elapsed_minutes} minutes, {elapsed_seconds} seconds")
+
+    except pytube.exceptions.PytubeError as e:
+        print("An error occurred:", str(e))
+    except Exception as e:
+        print("An unexpected error occurred:", str(e))
 
 
-try:
-    start_time = time.time()
-    download_with_progress(video_stream, f"{download_path}/{title}.mp4")
-    end_time = time.time()
-
-    total_time = end_time - start_time
-
-    # Convert total time to minutes and seconds
-    elapsed_minutes = int(total_time // 60)
-    elapsed_seconds = int(total_time % 60)
-
-    print("Download Completed!")
-    print(f"Download time: {elapsed_minutes} minutes, {elapsed_seconds} seconds")
-
-except pytube.exceptions.PytubeError as e:
-    print("An error occurred:", str(e))
-except Exception as e:
-    print("An unexpected error occurred:", str(e))
+if __name__ == "__main__":
+    main()
